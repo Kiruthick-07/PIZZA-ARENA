6dd9214fd1973c3a8d75ebfe1d14296c1087f437
Kiruthick-07 <tamilelectroniv@gmail.com>
2024-11-20 17:22:36 +0530

    image toggle

---
+++ index.js
@@ -0,0 +1,10 @@
+var indexval = 1;
+showImg(indexval);
+function slide(e){
+    showImg(indexval +=e);
+}
+function showImg(e){
+    var i;
+    const img = document.getElementsByClassName("mainimg1")
+
+}
